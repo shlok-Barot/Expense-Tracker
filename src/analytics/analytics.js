598dc9d318824f50d07f8a53861df4ade98eabc0
Shlok Barot <67185218+shlok-Barot@users.noreply.github.com>
2023-02-04 13:19:14 +0530

    Update analytics.js

--- src/analytics/analytics.js
+++ src/analytics/analytics.js
@@ -2,7 +2,7 @@ import ReactGA from "react-ga";
 
 export const initGA = () => {
   // Add your tracking ID created from https://analytics.google.com/analytics/web/#home/
-  ReactGA.initialize("UA-123071607-1");
+  ReactGA.initialize("G-CCS4SKN9KT");
 };
 
 export const logPageView = () => {
